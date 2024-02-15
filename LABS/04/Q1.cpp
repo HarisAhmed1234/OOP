1150df88ed5bed46a631b0f045c41081a770a2be
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-02-15 15:18:43 +0500

    Update Q1.cpp

--- LABS/04/Q1.cpp
+++ LABS/04/Q1.cpp
@@ -3,59 +3,47 @@
 
 using namespace std;
 
-class Weekdays {
+class Books {
 private:
-    string days[7];
-    int currDay;
+    string name;
+    string author;
+    string isbn;
+    int total_pages;
+    int pages_read;
 
 public:
-    Weekdays() {
-        days[0] = "Sunday"; days[1] = "Monday"; days[2] = "Tuesday"; 
-        days[3] = "Wednesday"; days[4] = "Thursday"; days[5] = "Friday"; 
-        days[6] = "Saturday";
-        currDay = 0;
+    Books() {
+        name = "";
+        author = "";
+        isbn = "";
+        total_pages = 0;
+        pages_read = 0;
     }
 
-    Weekdays(int currentDay) {
-        *this = Weekdays();
-        if (currentDay > 6)
-            currDay = currentDay % 7;
-        else
-            currDay = currentDay;
+    Books(string naam, string musanif, string aisbn, int totalpages, int pagesread) {
+        name = naam;
+        author = musanif;
+        isbn = aisbn;
+        total_pages = totalpages;
+        pages_read = pagesread;
     }
 
-    string getCurrentDay() {
-        return days[currDay];
-    }
-
-    string getNextDay() {
-        return days[(currDay + 1) % 7];
-    }
-
-    string getPreviousDay() {
-        return days[(currDay - 1 + 7) % 7];
-    }
-
-    string getNthDayFromToday(int n) {
-        return days[(currDay + n) % 7];
+    void read() {
+        if (pages_read < total_pages) {
+            pages_read++;
+            cout << "Page parh lia hai tumne agay barho. Page number: "<<pages_read <<endl;
+        } else {
+            cout << "Book khatam ho gayi hai!"<<endl;
+        }
     }
 };
 
 int main() {
-    int currentDay;
-    cout << "Enter the current day (0 for Sunday, 1 for Monday, ..., 6 for Saturday): ";
-    cin >> currentDay;
-
-    Weekdays obj(currentDay);
-
-    cout << "Current Day: " << obj.getCurrentDay() << endl;
-    cout << "Next Day: " << obj.getNextDay() << endl;
-    cout << "Previous Day: " << obj.getPreviousDay() << endl;
-
-    int n;
-    cout << "Enter the value of n for nth day from today: ";
-    cin >> n;
-    cout << "Nth Day from Today (" << n << "): " << obj.getNthDayFromToday(n) << endl;
-
+    Books book1("LORD OF THE RINGS", "HARIS AHMED", "1234", 4, 0);
+    book1.read(); 
+    book1.read(); 
+    book1.read(); 
+    book1.read(); 
+    book1.read(); 
     return 0;
 }
