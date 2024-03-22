8133086f8c6d2b3bcf2779fd97b2d21dd58baabd
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-03-22 11:53:51 +0500

    Create Q4.cpp

---
+++ LABS/06/Q4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+inline double calculateBattingAverage(int runs, int outs) {
+    if (outs == 0) {
+        return 0; 
+    }
+    return static_cast<double>(runs) / outs;
+}
+
+inline double calculateStrikeRate(int runs, int balls) {
+    if (balls == 0) {
+        return 0; 
+    }
+    return (static_cast<double>(runs) / balls) * 100;
+}
+
+int main() {
+    int runs, outs, balls;
+    
+    cout << "Enter total runs scored: ";
+    cin >> runs;
+    cout << "Enter number of times out: ";
+    cin >> outs;
+
+    cout << "Enter total balls faced: ";
+    cin >> balls;
+
+    double battingAverage = calculateBattingAverage(runs, outs);
+    
+    double strikeRate = calculateStrikeRate(runs, balls);
+    
+    cout << "Batting Average: " << battingAverage << endl;
+    cout << "Strike Rate: " << strikeRate << endl;
+
+    return 0;
+}
