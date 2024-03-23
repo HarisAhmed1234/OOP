2cf3cef04d78d218cd461f823ab157b99e50f6bc
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-03-24 03:11:25 +0500

    Create Q3.cpp

---
+++ LABS/08/Q3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Position{
+	public:
+	int X,Y,Z;
+	Position(int x, int y, int z){
+		X=x;
+		Y=y;
+		Z=z;
+	}
+};
+
+class Health{
+	public:
+	int Healthh;
+	Health(int health){
+		Healthh=health;
+	}
+};
+
+class Character: public Position, public Health{
+	public:
+		string Name;
+		int Age;
+		Character(string name,int age,int x, int y, int z,int health):Position(x,y,z),Health(health){
+			Name=name;
+			Age=age;
+		}
+		void Display(){
+			cout<<"Characters name: "<<Name<<endl;
+			cout<<"Characters Age: "<<Age<<endl;
+			cout<<"Character is standing on "<<X<<"i "<<Y<<"j "<<Z<<"k Axis"<<endl;
+			cout<<"Characters Health is: " << Healthh<<endl;
+		}
+};
+
+int main(){
+	Character c1("Haris",19,3,4,5,72);
+	c1.Display();
+}
