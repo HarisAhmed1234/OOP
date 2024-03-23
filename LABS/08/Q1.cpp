aec1379266c832f0a33aefecc49fab71ec21de71
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-03-24 03:09:40 +0500

    Update Q1.cpp

--- LABS/08/Q1.cpp
+++ LABS/08/Q1.cpp
@@ -1,1 +1,43 @@
 #include <iostream>
+using namespace std;
+
+class Book{
+public:
+	string Title;
+	string Author;
+	string Publisher;
+	Book(string title, string author, string publisher){
+		Title=title;
+		Author=author;
+		Publisher=publisher;
+	}
+	void BookDisplay(){
+		cout<<"Title: "<<Title<<endl;
+		cout<<"Author: "<<Author<<endl;
+		cout<<"Publisher: "<<Publisher<<endl;
+	}
+};
+
+class FictionBook: public Book{
+	public:
+	string Genre;
+	string Protagonist;
+	FictionBook(string title, string author, string publisher,string genre,string protagonist):
+		Book(title,author,publisher){
+			Genre=genre;
+			Protagonist=protagonist;
+		}
+		void FictionBookDisplay(){
+		cout<<"Title: "<<Title<<endl;
+		cout<<"Author: "<<Author<<endl;
+		cout<<"Publisher: "<<Publisher<<endl;
+		cout<<"Genre: "<<Genre<<endl;
+		cout<<"Protagonist: "<<Protagonist<<endl;
+		
+	}
+};
+
+int main(){
+	FictionBook book1("HERE IS 2024","Haris","Ali","Thriller","Haris");
+	book1.FictionBookDisplay();
+}
