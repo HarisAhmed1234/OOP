d472a851bd7c16302f110df60955141f48b5873f
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-05-10 20:31:01 +0500

    Update Q1.cpp

--- LABS/11/Q1.cpp
+++ LABS/11/Q1.cpp
@@ -1,1 +1,46 @@
 #include <iostream>
+
+using namespace std;
+
+class Convert {
+protected:
+    float val1;
+    float val2;
+public:
+    Convert(float v1):val1(v1),val2(0.0) {}
+    
+    virtual void compute() = 0;
+};
+
+class LitersToGallons:public Convert {
+public:
+    LitersToGallons(float v1) :Convert(v1) {}
+    
+    void compute() override {
+        val2=val1 / 3.78541;
+        cout<<"Resultt: " <<val1<<" liters equals "<<val2 << " gallons." <<endl;
+    }
+};
+
+class FahrenheitToCelsius:public Convert {
+public:
+    FahrenheitToCelsius(float v1):Convert(v1) {}
+    
+    void compute() override {
+        val2 = (val1 - 32) *5/9;
+        cout<<"Result: "<<val1<<" Fahrenheit equals "<<val2<<" Celsius." <<endl;
+    }
+};
+
+int main() {
+    Convert *convert1 = new LitersToGallons(4);
+    convert1->compute();
+
+    Convert *convert2 = new FahrenheitToCelsius(70);
+    convert2->compute();
+
+    delete convert1;
+    delete convert2;
+
+    return 0;
+}
